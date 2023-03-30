dab9d735aae541c1e8aeee5c0d9ea6336536d359
kishankushwaha8866664200 <84864817+kishankushwaha8866664200@users.noreply.github.com>
2023-03-30 13:47:50 +0530

    Add files via upload

---
+++ object3/object.js
@@ -0,0 +1,19 @@
+img = "";
+
+function preload() {
+    img = loadImage('phone.jpeg');
+}
+function setup() {
+    canvas = createCanvas(500, 500);
+    canvas.center();
+}
+
+function draw() {
+    image(img, 0, 0, 500, 500);
+
+    stroke('#ff0000');
+    fill('#ff0000');// to set text color to red
+    text("Phone", 150, 30);// to display text in canvas
+    noFill();// to make fill transparant
+    rect(140,10, 250, 480);
+}
