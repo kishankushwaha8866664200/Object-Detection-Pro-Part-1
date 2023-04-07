a762d77f5ad36bbd110945a7948a23e5865bfc32
kishankushwaha8866664200 <84864817+kishankushwaha8866664200@users.noreply.github.com>
2023-04-07 17:54:59 +0530

    Update object.js

--- object3/object.js
+++ object3/object.js
@@ -1,11 +1,26 @@
 img = "";
-
+var Status;
 function preload() {
     img = loadImage('phone.jpeg');
 }
-function setup() {
-    canvas = createCanvas(500, 500);
-    canvas.center();
+function setup(){
+    canvas = createCanvas(640,350);
+    canvas.position(315,200);
+    object_Detector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!");
+    Status = true;
+    object_Detector.detect(img,gotResults);
+}
+
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }
+    console.log(results);
 }
 
 function draw() {
